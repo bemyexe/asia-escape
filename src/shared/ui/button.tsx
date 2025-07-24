02b28c5496f04d0430930c3bfb5035a251bc0c2e
maxim <trifonov.max@mail.ru>
2025-07-24 20:20:24 +0300

    feat: button component

---
+++ src/shared/ui/button.tsx
@@ -0,0 +1,21 @@
+import {cn} from '../utils';
+
+interface Props extends React.ComponentProps<'button'> {
+  onClick: () => void;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Button = ({onClick, className, children, ...props}: Props) => {
+  return (
+    <button
+      className={cn(
+        'border rounded p-0.5 bg-blue-600 text-amber-50 cursor-pointer hover:bg-blue-800 active:scale-95 transition duration-300 ease-in-out',
+        className
+      )}
+      onClick={onClick}
+      {...props}>
+      {children}
+    </button>
+  );
+};
